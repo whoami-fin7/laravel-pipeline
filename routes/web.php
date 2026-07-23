34d89a11c2a69397fe398d848f7a7097899183ce
Alexsandro Guerreiro <aguerreiro.manaus@gmail.com>
2026-07-23 17:07:57 -0300

    teste pipeline hml

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Olá, aplicação de teste em Producao";
+    return "Olá, aplicação de teste em Producao - GIT Teste";
 });
